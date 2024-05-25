4a06e64a59b67a4b4642140ccde65865f9922f3e
maks120913 <168225285+maks120913@users.noreply.github.com>
2024-05-25 20:49:25 +0300

    main.py

--- main.py
+++ main.py
@@ -7,24 +7,24 @@ class Vehicle:
     def display_info(self):
         print(f"Make: {self.make}, Model: {self.model}, Mileage: {self.mileage} miles")
 
-    def drive(self, miles):
-        if miles > 0:
-            self.mileage += miles
-        else:
-            print("Fals")
+    def drive(self, miles=0):
+        self.mileage = miles
+       miles = int(input("Введіть на скільки збільшився пробіг машини:"))
+       result = self.mileage + miles
+       print("Загальний пробіг:", result)
 
     def maintenance_needed(self):
-        return self.mileage > 100.000
+       if self.mileage >= 100000:
+           print(True)
+       else:
+           print(False)
 
     def reset_mileage(self):
         self.mileage = 0
+        print("Пробіг дорівнює нулю")
 
-vehicle = Vehicle("Audi", "R8", 6800)
+vehicle = Vehicle("Audi", "R8", 0)
 vehicle.display_info()
-vehicle.drive(120)
-vehicle.display_info()
-print(vehicle.maintenance_needed())
-vehicle.drive(6800)
-print(vehicle.maintenance_needed())
+vehicle.drive()
+vehicle..maintenance_needed()
 vehicle.reset_mileage()
-vehicle.display_info()
